b11da011b39b701562a636789d040e71acb62804
jesscov12 <110857920+jesscov12@users.noreply.github.com>
2022-12-27 15:41:10 -0700

    adjusting css properties

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Search from "./Search";
 import Weather from "./Weather";
-import Current from "./Current";
 
 import "./App.css";
 import "./Weather.css";
-import "./Current.css";
 import "./Search.css";
 
 export default function App() {
@@ -14,9 +12,8 @@ export default function App() {
       <div className="container">
         <Search />
         <h1>Austin</h1>
-        <div className="row">
+        <div>
           <Weather />
-          <Current />
         </div>
       </div>
       <footer>
--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,32 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="Weather col-6">
-      <h2>66°</h2>
-      <span className="celsius">℃ |</span>
+    <div className="Weather">
+      <div className="row">
+        <div className="col-6">
+          <h2>66°</h2>
+          <span className="celsius">℃ |</span>
 
-      <span className="fahrenheit"> ℉</span>
-      <br />
-      <img src="http://openweathermap.org/img/wn/50d@2x.png" id="icon" alt="" />
+          <span className="fahrenheit"> ℉</span>
+          <br />
+          <img
+            src="http://openweathermap.org/img/wn/50d@2x.png"
+            id="icon"
+            alt=""
+          />
+        </div>
+        <div className="col-6">
+          <h3>Currently</h3>
+          <ul>
+            <li className="date">12/2/2022</li>
+            <li className="time">11:25 am</li>
+            <li className="description"> Mist</li>
+            <li className="speed">4 mph</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
