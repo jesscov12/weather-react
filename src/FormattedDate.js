43b85e50717837820d810a1434e0e9fb3c187f98
jesscov12 <110857920+jesscov12@users.noreply.github.com>
2023-01-17 15:41:35 -0600

    used useEffect to update real time weather forecast

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -5,13 +5,28 @@ export default function FormattedDate(props) {
   let month = months[props.date.getMonth()];
   let day = props.date.getDate();
   let year = props.date.getFullYear();
-  let timeString = props.date.toLocaleTimeString(`en-US`);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let date = days[props.date.getDay()];
+  let timeString = props.date.toLocaleTimeString(`en-US`, {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
   if (day < 10) {
     day = `0${day}`;
   }
 
   return (
     <div>
+      {date}
+      <br />
       {month}/{day}/{year}
       <br />
       {timeString}
