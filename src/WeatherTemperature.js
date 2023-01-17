0b3a201770646267e256218fc5c161464f4969b2
jesscov12 <110857920+jesscov12@users.noreply.github.com>
2023-01-17 15:49:16 -0600

    got rid of celsius and Fahrenheit conversions

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,43 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("fahrenheit");
-
-  function convertToFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  function convertToCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-  if (unit === "fahrenheit") {
-    return (
-      <div className="WeatherTemperature">
-        <h2>{Math.round(props.fahrenheit)}°F</h2>
-        <span className="unit">
-          <button onClick={convertToCelsius}> ℃ |</button>
-
-          <button className="fs-6">
-            <strong>℉</strong>{" "}
-          </button>
-        </span>
-      </div>
-    );
-  } else {
-    let celsius = ((props.fahrenheit - 32) * 5) / 9;
-    return (
-      <div className="WeatherTemperature">
-        <h2>{Math.round(celsius)}°C</h2>
-        <span className="unit">
-          <button className="fs-6">
-            {" "}
-            <strong>℃</strong> |
-          </button>
-
-          <button onClick={convertToFahrenheit}>℉ </button>
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <h2>{Math.round(props.fahrenheit)}°F</h2>
+    </div>
+  );
 }
